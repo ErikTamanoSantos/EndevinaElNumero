e39f99874baf2610c4f29cea3a0810ccea668718
eriktamano <eriktamano@gmail.com>
2022-09-23 19:22:30 +0200

    contador afegit

--- app/src/main/java/com/example/endevinaelnumero/MainActivity.java
+++ app/src/main/java/com/example/endevinaelnumero/MainActivity.java
@@ -7,30 +7,39 @@ import android.util.SparseIntArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int num = (int) (Math.random()*100);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int num = (int) (Math.random()*100);
-
         final Button button = findViewById(R.id.button);
-        final EditText text = findViewById(R.id.editTextNumber);
+        final EditText entrada = findViewById(R.id.editTextNumber);
+
+
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (Integer.parseInt(text.getText().toString()) < num) {
-                    Toast.makeText(MainActivity.this, "El numero insertat es menor", Toast.LENGTH_SHORT).show();
-                } else if (Integer.parseInt(text.getText().toString()) > num) {
-                    Toast.makeText(MainActivity.this, "El numero insertat es major", Toast.LENGTH_SHORT).show();
+
+                final TextView textContador = findViewById(R.id.Contador);
+                final TextView resp = findViewById(R.id.Respuesta);
+
+                textContador.setText(String.valueOf(Integer.parseInt(textContador.getText().toString()) + 1));
+
+
+                if (Integer.parseInt(entrada.getText().toString()) < num) {
+                    resp.append("\nEl numero insertat és menor");
+                } else if (Integer.parseInt(entrada.getText().toString()) > num) {
+                    resp.append("\nEl numero insertat és major");;
                 } else {
-                    Toast.makeText(MainActivity.this, "Felicitats, has endevinat el numero", Toast.LENGTH_SHORT).show();
+                    resp.append("\nFelicitats, has endevinat el numero");
                 }
-                text.setText("");
+                entrada.setText("");
             }
         });
 
