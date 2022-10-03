aae51540a2870945be1f57f0742d2afa48f8f9fd
eriktamano <eriktamano@gmail.com>
2022-10-03 16:28:20 +0200

    Main Activity acabat

--- app/src/main/java/com/example/endevinaelnumero/MainActivity.java
+++ app/src/main/java/com/example/endevinaelnumero/MainActivity.java
@@ -2,46 +2,87 @@ package com.example.endevinaelnumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.SparseIntArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
-    int num = (int) (Math.random()*100);
-    @Override
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    int num = (int) (Math.random()*10);
+
+    public ArrayList<Record> records = new ArrayList<Record>();
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.button);
+        final Button submit = findViewById(R.id.button);
+        final Button hof = findViewById(R.id.HOFButton);
         final EditText entrada = findViewById(R.id.editTextNumber);
 
+        final TextView hist = findViewById(R.id.Respuesta);
+        final TextView textContador = findViewById(R.id.Contador);
+
+        hist.setMovementMethod(new ScrollingMovementMethod());
 
 
-        button.setOnClickListener(new View.OnClickListener() {
+        submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-                final TextView textContador = findViewById(R.id.Contador);
-                final TextView resp = findViewById(R.id.Respuesta);
 
                 textContador.setText(String.valueOf(Integer.parseInt(textContador.getText().toString()) + 1));
 
+                int resp = Integer.parseInt(entrada.getText().toString());
 
-                if (Integer.parseInt(entrada.getText().toString()) < num) {
-                    resp.append("\nEl numero insertat és menor");
-                } else if (Integer.parseInt(entrada.getText().toString()) > num) {
-                    resp.append("\nEl numero insertat és major");;
+                if (resp < num) {
+                    hist.append("\nEl numero " + resp + " és menor");
+                } else if (resp > num) {
+                    hist.append("\nEl numero " + resp + " és major");;
                 } else {
-                    resp.append("\nFelicitats, has endevinat el numero");
+                    final EditText jugador = new EditText(MainActivity.this);
+                    AlertDialog.Builder popup = new AlertDialog.Builder(MainActivity.this);
+                    popup.setTitle("Felicitats, has endevinat el número!\nInserta el teu nom.");
+                    popup.setView(jugador);
+                    popup.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            records.add(new Record(jugador.getText().toString(), Integer.parseInt(textContador.getText().toString())));
+                            textContador.setText("0");
+                            entrada.setText("");
+                            hist.setText("Historial:");
+                            num = (int) (Math.random()*10);
+                        }
+                    });
+                    popup.create();
+                    popup.show();
                 }
-                entrada.setText("");
+            }
+        });
+
+        hof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RecordsActivity.class);
+                startActivity(intent);
             }
         });
 
     }
+}
+
+class WinnersActivity extends AppCompatActivity {
+
 }
