c32f9ae1e9d2233d98e6cb095f0809035015e5d5
eriktamano <eriktamano@gmail.com>
2022-11-03 15:32:09 +0100

    Record afegit

---
+++ app/src/main/java/com/example/endevinaelnumero/Record.java
@@ -0,0 +1,11 @@
+package com.example.endevinaelnumero;
+
+public class Record {
+    public String nom;
+    public int punts;
+
+    public Record(String nom, int punts) {
+        this.punts = punts;
+        this.nom = nom;
+    }
+}
